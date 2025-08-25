e3a3c43f001472196d6d391899eed01d26785b1b
karthik70412 <dokkukarthik704@gmail.com>
2025-08-25 12:07:52 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {formData:{
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      country: "",
+      dob: "",
+      password: "",
+      confirmpassword:"",
+      terms: false,
+    }, errMsg:{}};
+  }
+  
+  handleSubmit(){
+    alert("Testing...");
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  render() {
+    const{formData, errMsg} = this.state;
+    return (
+      <>
+        <header>
+          <div className='logo'>Validation</div>
+        </header>
+        <section>
+          <div className='content'>
+            <h1>Create Account</h1>
+            <div className='grid'>
+              <div>
+                <label>First Name *</label>
+                <input type='text' name='firstName' value={formData.firstName} />
+                <p></p>
+              </div>
+              <div>
+                <label>Last Name *</label>
+                <input type='text' name='lastName' value={formData.lastName} />
+                <p></p>
+              </div>
+            </div>
+
+            <div className='grid'>
+              <div>
+                <label>Email ID *</label>
+                <input type='text' name='email' value={formData.email} />
+                <p></p>
+              </div>
+              <div>
+                <label>Phone Number *</label>
+                <input type='text' name='phone' value={formData.phone} />
+                <p></p>
+              </div>
+            </div>
+
+            <div className='grid'>
+              <div>
+                <label>Select Country *</label>
+                <select name='country' value={formData.country}>
+                  <option>Select..</option>
+                  <option>India</option>
+                  <option>USA</option>
+                  <option>Europe</option>
+                </select>
+                <p></p>
+              </div>
+              <div>
+                <label>Date of Birth *</label>
+                <input type='date' name='dob' value={formData.dob} />
+                <p></p>
+              </div>
+            </div>
+
+            <div className='grid'>
+              <div>
+                <label>Password *</label>
+                <input type='password' name='password' value={formData.password} />
+                <p></p>
+              </div>
+              <div>
+                <label>Confirm Password *</label>
+                <input type='password' name='confirmpassword' value={formData.confirmpassword} />
+                <p></p>
+              </div>
+            </div>
+
+            <div className='terms'>
+              <input type='checkbox' name='terms' value={formData.terms} />
+              <label>I agree the Terms and Conditions</label>
+              <p></p>
+            </div>
+
+            <div className='submission'>
+              <button onClick={()=>this.handleSubmit()}>Register</button>
+            </div>
+          </div>
+        </section>
+        <footer>Copyright @ 2025. All rights reserved.</footer>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
